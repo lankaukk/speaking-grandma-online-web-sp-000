090f316d9172ea8c5b859d32fbb8a4f90ae75605
McKayla Lankau <lankaukk@yahoo.com>
2020-07-27 21:55:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
-    return ""
+    return "HUH?! SPEAK UP, SONNY!"
   if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   if phrase == "I LOVE YOU GRANDMA!"
